d38b6b5bec7ff140acb142b0669ecb10976426bf
Mikael-H01 <83763858+Mikael-H01@users.noreply.github.com>
2023-04-13 11:04:41 +0200

    Add files via upload

---
+++ MultiAgentRL_Thesis-main/DQN-final_v3/MultiAgentRL_Thesis-main/MainLoop_Run.py
@@ -0,0 +1,61 @@
+from WarehouseEnv import WarehouseEnv
+import pygame
+from levels import *
+import itertools
+import random
+import numpy as np
+
+def main():
+    #Parameters doesnt do anything but are required to use the environment
+    gamma=0.95 #Discountfactor
+    batch_size = 50 #Amount of sampled transitions from replaybuffer
+    buffer_size = 15000 #Max number of transitions stored
+    min_replay_size = 75000  #Minimum amount of transitions in replay before training
+    eps = 0 #Want deterministic while running with trained networks
+    eps_min = 0.1
+    eps_dec = 500000
+    update_freq = 150 #How often targetparameters are copied fron online
+    pygame.init()
+
+    BLOCK_SIZE = 20
+    NUM_AGENTS = 2
+    FPS = 5
+
+    warehouse = WarehouseEnv(test_lvl32, BLOCK_SIZE, NUM_AGENTS, gamma, batch_size, buffer_size,
+                  min_replay_size, eps, eps_min, eps_dec, update_freq, "test_lvl32") #Environment initialized
+    clock = pygame.time.Clock()
+    playing = True
+
+    for agent in warehouse.agents:  #Load the trained networks for current settings
+        agent.load_model()
+
+    warehouse.reset() #Reset the warehouse before starting learning
+    episode = 1
+    while playing:
+        for event in pygame.event.get():
+            for step in itertools.count(): #Counts how many states has been visited
+                clock.tick(FPS)
+                for agent in warehouse.agents: #For each agent, perform actions
+                    if agent.done == False: #If agent done, stop making decisions
+                        action = agent.net_online.act(agent.state)#Deterministic acting after training
+                        rew, new_state, agent.done = warehouse.step(action[0], agent) #Take step, save reward, new state and if done
+
+                        agent.episode_reward += rew #Save reward for current episode
+                done_check = []
+                for agent in warehouse.agents: #Checks if any agent still working
+                    done_check.append(agent.done)
+                if False not in done_check: #If not, reset environment and save episodereward to memory
+                    episode += 1
+                    warehouse.reset()
+                    print("Episode "+str(episode))
+                    for agent in warehouse.agents:
+                        print("Agent "+str(agent.index))
+                        print("Successful Deliveries: " + str(agent.delivered_packages), "Crashes: " + str(agent.crashes))
+                        agent.rew_buffer.append(agent.episode_reward)
+                        agent.episode_reward = 0.0
+                warehouse.render()
+    pygame.quit()
+            
+
+if __name__ =='__main__':
+    main()
---
+++ MultiAgentRL_Thesis-main/DQN-final_v3/MultiAgentRL_Thesis-main/levels.py
@@ -0,0 +1,139 @@
+level = [
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+    "W                              DD   W",
+    "W         WWWWWW               DD   W",
+    "W   WWWW       W         WWW        W",
+    "W   W        WWWW                   W",
+    "W WWW  WWWW                         W",
+    "W   W     WW       WWW     WWW      W",
+    "W   W     W   WWWW    W             W",
+    "W   WWW WWW     W      W            W",  
+    "W     W   W     W      W            W",   
+    "W WW   W   WW   WW     WWWWWWW      W",                
+    "W W      WW               WW        W",                 
+    "W W   WWWW    PPPPPPPPPPP           W",                 
+    "W     W       PP                    W",
+    "W                      WWWWW        W",
+    "W                                   W",   
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+    ]
+
+warehouse_lvl = [
+    "WWWWWWWWWW",
+    "WPWPWPW  W",
+    "W W W W  W",
+    "W W W W  W",
+    "W W W W  W",
+    "W W W W  W",
+    "W W W W  W",
+    "W W W W  W",
+    "W        W",
+    "W D  D  DW",
+    "WWWWWWWWWW"
+]
+
+basic_lvl = [
+    "W-W-W-W-W-W-W-W-W-W",
+    "W                 W",
+    "W                 W",
+    "W                 W",
+    "W                 W",
+    "W                 W",
+    "W                 W",
+    "W                 W",
+    "W                 W",
+    "W-W-W-W-W-W-W-W-W-W" 
+]
+
+test_lvl = [
+    "WWWWWWWWWWWWWWWW",
+    "W              W",
+    "W              W",
+    "W              W",
+    "W   P         DW",
+    "W              W",
+    "W              W",
+    "W              W",
+    "W              W",
+    "WWWWWWWWWWWWWWWW" 
+]
+
+test_lvl3 = [
+    "WWWWWWWWW",
+    "W       W",
+    "W  P    W",
+    "W       W",
+    "W       W",
+    "W       W",
+    "W    D  W",
+    "W       W",
+    "WWWWWWWWW" 
+]
+
+test_lvl32 = [
+    "WWWWWWWWW",
+    "W       W",
+    "W P   D W",
+    "W       W",
+    "W       W",
+    "W       W",
+    "W D   P W",
+    "W       W",
+    "WWWWWWWWW" 
+]
+
+test_lvl3_wall = [
+    "WWWWWWWWW",
+    "W       W",
+    "W  P    W",
+    "W       W",
+    "W WWWWW W",
+    "W       W",
+    "W    D  W",
+    "W       W",
+    "WWWWWWWWW" 
+]
+
+test_lvl2 = [
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+    "W                               W",
+    "W                               W",
+    "W                               W",
+    "W                               W",
+    "W                               W",
+    "W                               W",
+    "WP                             DW",
+    "W                               W",
+    "W                               W",
+    "W                               W",
+    "W                               W",
+    "W                               W",
+    "W                               W",
+    "W                               W",
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW" 
+]
+
+test_lvl20x20 = [
+    "WWWWWWWWWWWWWWWWWWWWWW",
+    "W  DD            DD  W",
+    "W                    W",
+    "W  WWWWWWW  WWWWWWW  W",
+    "W   P                W",
+    "W              P     W",
+    "W  WWWWWWW  WWWWWWW  W",
+    "W      P             W",
+    "W              P     W",
+    "W  WWWWWWW  WWWWWWW  W",
+    "WD                  DW",
+    "WD      P           DW",
+    "W  WWWWWWW  WWWWWWW  W",
+    "W                P   W",
+    "W                    W",
+    "W  WWWWWWW  WWWWWWW  W",
+    "W     P              W",
+    "W                    W",
+    "W  WWWWWWW  WWWWWWW  W",
+    "W             P      W",
+    "W  DD            DD  W",
+    "WWWWWWWWWWWWWWWWWWWWWW" 
+]
