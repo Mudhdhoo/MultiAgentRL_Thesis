b02054a2d224bea062620214cc59478bf6d37de0
Mikael-H01 <83763858+Mikael-H01@users.noreply.github.com>
2023-05-23 15:01:52 +0200

    Delete MultiAgentRL_Thesis-main/DQN-final_v2/MultiAgentRL_Thesis-main directory

--- MultiAgentRL_Thesis-main/DQN-final_v2/MultiAgentRL_Thesis-main/Wall.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-class WallElement:
-    def __init__(self, wall_pos:tuple, wall_size:tuple, texture) -> None:
-        self.wall_element = pygame.Rect(wall_pos[0], wall_pos[1], wall_size, wall_size)
-        self.texture = texture
